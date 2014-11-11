5465a2356a22481f8230de3ca1de956b65a07b09
Justin Park <jpark3000@gmail.com>
2014-11-11 00:13:41 -0600

    Converts USD formatted string to integer and vice versa
    Prices are being read from a file as a formatted string
    i.e. "$4.25". Converting that to an integer and multiplying by 100
    will make it easier to operate on.
    
    Conversion back to a USD formatted string is for display and output
    purposes.

---
+++ spec/currency_utils_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+require 'currency_utils'
+
+describe CurrencyUtils do
+  it 'converts a USD formatted string into an integer' do
+    expect(CurrencyUtils.currency_to_i('$2.25')).to eq 225
+  end
+
+  it 'converts an integer into a USD formatted string' do
+    expect(CurrencyUtils.integer_to_currency(1445)).to eq '$14.45'
+  end
+
+  it 'converts an integer into a USD formatted string' do
+    expect(CurrencyUtils.integer_to_currency(100)).to eq '$1.00'
+  end
+
+  it 'converts an integer into a USD formatted string' do
+    expect(CurrencyUtils.integer_to_currency(25)).to eq '$0.25'
+  end
+end
